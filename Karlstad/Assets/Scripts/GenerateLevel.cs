d7d83751481aa8739d29f1e636965510d1a442f3
OskarPalmen <oskarpalmen00@gmail.com>
2023-03-16 16:20:35 +0100

    addedstuff

--- Karlstad/Assets/Scripts/GenerateLevel.cs
+++ Karlstad/Assets/Scripts/GenerateLevel.cs
@@ -9,10 +9,12 @@ public class GenerateLevel : MonoBehaviour
     public bool creatingSection = false;
     public int secNum;
 
+    private List<GameObject> spawnedSections = new List<GameObject>(); // store references to spawned sections
+
     // Update is called once per frame
     void Update()
     {
-       if(creatingSection == false)
+        if (creatingSection == false)
         {
             creatingSection = true;
             StartCoroutine(GenerateSection());
@@ -22,9 +24,18 @@ public class GenerateLevel : MonoBehaviour
     IEnumerator GenerateSection()
     {
         secNum = Random.Range(0, 3);
-        Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        GameObject newSection = Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        spawnedSections.Add(newSection); // add reference to spawned section to the list
         zPos += 25;
         yield return new WaitForSeconds(1);
         creatingSection = false;
+
+        // destroy clones one at a time more quickly
+        float delayTime = 3f; // decrease the delay time
+        for (int i = 0; i < spawnedSections.Count; i++)
+        {
+            yield return new WaitForSeconds(delayTime); // wait for a shorter time before destroying the next clone
+            Destroy(spawnedSections[i]);
+        }
     }
 }
